b7188d9b6addb3186b81b9b49582c0fc49b82cbc
arunjethva <141201433+arunjethva@users.noreply.github.com>
2023-09-01 08:49:17 +0530

    Add files via upload

---
+++ modual 3-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+ printf(" name - Arun\n");
+ printf("birth date -19-8-2003\n");
+ printf("age - 20\n");
+ printf("address - dwarkadhish society,kosad ,surat");
+
+}
---
+++ modual 3_6_day_year.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int  years,days;
+
+    printf("Enter The number of years : ");
+    scanf("%d",&years);
+
+    days=365*years;
+
+    printf("The number of day is : %d\n",days);
+
+
+  printf("Enter The Days number  : ");
+  scanf("%d",&days);
+
+  years=days/365;
+
+  printf("Number of years is : %d\n",years);
+  
+    return 0;
+}
